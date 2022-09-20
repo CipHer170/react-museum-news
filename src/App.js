9750f5f7abeb480c76eb399524331f58dd8d0f49
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2022-09-20 17:09:56 +0500

    museum without mobile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,172 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// import "./media.css";
+import newsImage from "./images/cat.jpg";
+import { HiLocationMarker } from "react-icons/hi";
+import { BsClockFill } from "react-icons/bs";
+
+const descriptionText = ` 1927 yilda Uzbekiston ustalarining eng yaxshi ijod namunalari
+kurgazmasi tashkil qilingan edi. So'ng bu doimiy kurgazmaga
+aylantirilib, "O'zbekiston xalq xujaligi kurgazmasi" nomini
+oldi. Kurgazmaga quyilgan eksponatlar yilma-yil yigilib bordi.
+SHu bilan birga ilgarigi davrlarda tayyorlangan, qo'lda tikilgan
+kashtalar, do'ppilar, zargarlik buyumlari, gilamlar va shunga
+o'xshash xalk amaliy san'ati namunalari ham sotib olindi. Ana
+shu to'plangan xalk amaliy san'at asarlari 1937 yilning 7
+iyulida Toshkentda ochilgan "Hunarmandchilik muzeyi" da namoyish
+etila boshlandi. 1960 yilda muzey "O'zbekiston amaliy san'ati
+doimiy kurgazmasi" nomini oldi. 1997 yilda sobiq ko'rgazma
+O'zbekiston Respublikasi Madaniyat ishlari vazirligi tasarrufiga
+o'tkazildi va "Davlat Amaliy san'at muzeyi" maqomiga ega buldi.
+Bugungi kunda muzey xazinasida XIX asrning birinchi yarmidan to
+hozirgi kunlarimizgacha bo'lgan davrni qamrab olgan qo'li gul
+ustalar tomonidan yaratilgan bebaho amaliy san'at durdonalari
+yig'ilgan. Muzey xazinasida saqlanayotgan xalq amaliy san'at
+asarlarini badiiy jihatdan uch guruhga bo'lish mumkin.
+Birinchidan, bu qadimiy an'analar asosida yaratilgan, har bir
+hudud ichidagi o'ziga xosligini aks ettirgan maktablarga oid
+amaliy san'at asarlari; Ikkinchidan, XX asrning ikkinchi
+yarmidan boshlab, xalq ustalari an'anaviy uslubini yo'qotmagan
+xolda uni o'z ijodiy hofizasi bilan rivojlantirib, yanada badiiy
+bezak bilan boyitib yaratilgan asarlardir. Uchinchi guruh esa,
+bugungi kun amaliy san'at taraqqiyotidan kelib chiqib,
+rang-barang turfa naqshlar bilan bezatilib, zamonaviy san'at
+talablari darajasidagi yetuk asarlardan iborat. Muzey zaxirasida
+to'plangan buyumlar 20 nomdagi kolleksiyaga ajratilib saqlanadi
+va shu turlar buyicha ekspozitsiyalarda namoyish qilinadi.
+O'zbek xalki o'zining qadimiy amaliy san'ati bilan jahonga
+mashxurdir. Zebi ziynatli buyumlar yasash san'ati xalqimiz
+jodining eng qadimiylaridan hisoblanadi. Bu nodir xalq badiiy
+hunarmandchiligi namunalarini me'moriy yodgorliklar, ganch,
+yog'och o'ymakorligi, naqqoshlikda, shuningdek, misgarlik,
+zargarlik, kulolchilik, gilamduzlik, ipak va ip gazlamachiligi,
+zarduzlik, kashtachilik va o'nlab boshqa amaliy san'at turlarida
+o'z namunalarini saqlab qolgan. Bugungi kunda yuksak va noyob
+san'at durdonalari darajasiga ko'tarilgan bu bebaho merosimiz
+muzeylarimizda saqlanib kelinmokda. O'zbekiston Respublikasi
+Prezidenti I.A.Karimov "Biz xalqni nomi bilan emas, balki
+madaniyati, ma'naviyati orqali bilamiz, tarixining
+tagtomirlarigacha nazar tashlaymiz"deb bejiz uqtirmagan edi.
+Darxaqiqat, Respublikamiz muzeylari xazinalarida saqlanayotgan
+moddiy va ma'naviy madaniyatimiz bebaho yodgorliklari bizning
+kim bo'lganligimizni namoyon etuvchi nodir guvohlardir.
+Mustaqilligimiz yillarida respublikamiz muzeylari faoliyatini
+tubdan yaxshilashga alohida e'tibor qaratilmokda. Muzeylar
+faoliyatini yaxshilash chora tadbirlari to'g'risida 1994 va 1998
+yillarda Vazirlar Maxkamasining ikkita qarori qabul
+qilinganligi, "o'zbekmuzey" jamg'armasi tuzilganligi, Toshkentda
+o'zbekiston tarixi davlat muzeyi, Temuriylar tarixi davlat
+muzeyi, "Qatag'on qurbonlari xotirasi", Termizda arxeologiya
+muzeylari tashkil etilganligi buning yaqqol ifodasidir.
+O'zbekiston Respublikasi Prezidentining 1998 jil 12 yanvardagi
+"Muzeylar faoliyatini tubdan yaxshilash va takomillashtirish
+tug'risda"gi Farmonida "O'zbekiston hududida qadimdan
+shakllangan muzeylar tizimini yanada takomillashtirish, ularning
+xalk ma'naviyaxloqiy kamolotida tutgan o'rnini oshirish, muzey
+fondlarida saqlanib kelinayotgan xalkimiz boy tarixini,
+mustaqilligimiz odimlarini aks ettiruvchi noyob, nodir
+eksponatlarni avaylab asrash, o'rganish, boyitib borish, dunyoga
+olib chiqish va targ'ib qilish, ulardan xalqimiz ongida milliy
+g'urur va iftixor, istiqlol va Vatanga sadoqat tuyg'ularini
+kuchaytirish yo'lida keng foydalanish" zarurligi ko'rsatib
+o'tiladi. Respublika Davlat Amaliy san'at muzeyida milliy
+madaniyatimizning uzviy qismi bo'lgan xalq amaliy san'atining
+eng noyob yodgorliklari saqlanadi. Muzey ekspozitsiyalarida xalq
+amaliy san'atining 50 dan ortiq turi namoyish etib kelinmoqda.
+Ma'lumki, O'zbekiston hududida asrlar davomida xalq badiiy
+hunarmandchiligining o'ziga hos yirik markazlari shakllangan.
+har bir hudud o'zining badiiy bezak san'ati bilan boshqalardan
+ajralib turgan. Hususan, CHust duppisi, pichog'i, Rishton
+o'zining lojuvard sopol idishlari, Marg'ilon kamalak tovlanma
+atlasi, Buxoro zarduzligi, SHahrixon pichoqchiligi bilan ma'lumu
+mashxurdir. Muzey zallarida sayr etar ekansiz, inson qo'li bilan
+yaratilgan mujizalarni, betakror san'at nafisligini ko'rib,
+olamolam zavk olasiz. Muzey o'z xazinasida saqlanayotgan 7
+mingdan ortiq xalq amaliy san'ati nodir asarlarining hammasini
+bir yula ekspozitsiyasida namoyish qilish imkoniga ega emas.`;
 
 function App() {
+  const [showMore, setShowMore] = useState(false);
+
+  // const showMoreNews = () => {
+  //   setShowMore(!showMore);
+  //   const news = document.getElementsByClassName("news");
+  //   news.body.style.backgroundColor = "red";
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="title">
+          Respublika bo'yicha
+          <h2 className="sub-title"> yangiliklar</h2>
+        </div>
+
+        {/* news */}
+        <div className="main-news">
+          <div className="news">
+            <div className="images">
+              <img src={newsImage} className="news-image" />
+            </div>
+
+            <div className="news-description">
+              <div className="news-description-info">
+                <div className="info">
+                  <h4 className="news-title">Muzey Yangiliklari</h4>
+                  <h4 className="news-location">
+                    <HiLocationMarker className="icon" />
+                    Amaliy san'at muzeyi
+                  </h4>
+                  <h4 className="news-updated-time">
+                    <BsClockFill className="icon" />
+                    day(week): time
+                  </h4>
+                </div>
+                <p className="description">
+                  {showMore
+                    ? descriptionText
+                    : descriptionText.substring(0, 189)}
+                </p>
+              </div>
+              <div className="news-time">
+                <p className="news-date">15</p>
+                <p className="news-month">sen</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="news">
+            <div className="images">
+              <img src={newsImage} className="news-image" />
+            </div>
+
+            <div className="news-description">
+              <div className="news-description-info">
+                <div className="info">
+                  <h4 className="news-title">Muzey Yangiliklari</h4>
+                  <h4 className="news-location">
+                    <HiLocationMarker className="icon" />
+                    Amaliy san'at muzeyi
+                  </h4>
+                  <h4 className="news-updated-time">
+                    <BsClockFill className="icon" />
+                    day(week): time
+                  </h4>
+                </div>
+                <p className="description">
+                  {showMore
+                    ? descriptionText
+                    : descriptionText.substring(0, 189)}
+                </p>
+              </div>
+              <div className="news-time">
+                <p className="news-date">15</p>
+                <p className="news-month">sen</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
