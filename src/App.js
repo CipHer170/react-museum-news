6e7a098ea68634309ec8e53196d868a8fd4c02ef
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2022-09-21 15:41:52 +0500

    dd

--- src/App.js
+++ src/App.js
@@ -95,71 +95,73 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container">
-        <div className="news__main__title">
-          Respublika bo'yicha
-          <h2 className="news__sub__title"> yangiliklar</h2>
-        </div>
-
-        {/* news */}
-        <div className="news__items">
-          <div className="news__item">
-            <div className="news__images">
-              <img src={newsImage} alt="museum" className="news-image" />
-            </div>
-
-            <div className="news__descriptions">
-              <div className="news__description__info">
-                <div className="news__description__timestamp">
-                  <h4 className="news-title">Muzey Yangiliklari</h4>
-                  <h4 className="news__location">
-                    <HiLocationMarker className="icon news__location__icon" />
-                    Amaliy san'at muzeyi
-                  </h4>
-                  <h4 className="news-updated-time">
-                    <BsClockFill className="icon " />
-                    day(week): time
-                  </h4>
-                </div>
-                <p className="news__description">
-                  {showMore
-                    ? descriptionText
-                    : descriptionText.substring(0, 189)}
-                </p>
-              </div>
-              <div className="news__description__time">
-                <p className="news__date">15</p>
-                <p className="news__month">sen</p>
-              </div>
-            </div>
+      <div className="base-contain">
+        <div className="news">
+          <div className="news__main__title">
+            Respublika bo'yicha
+            <h2 className="news__sub__title"> yangiliklar</h2>
           </div>
-          <div className="news__item">
-            <div className="news__images">
-              <img src={newsImage} alt="museum" className="news-image" />
-            </div>
 
-            <div className="news__descriptions">
-              <div className="news__description__info">
-                <div className="news__description__timestamp">
-                  <h4 className="news-title">Muzey Yangiliklari</h4>
-                  <h4 className="news__location">
-                    <HiLocationMarker className="icon news__location__icon" />
-                    Amaliy san'at muzeyi
-                  </h4>
-                  <h4 className="news-updated-time">
-                    <BsClockFill className="icon " />
-                    day(week): time
-                  </h4>
-                </div>
-                <p className="news__description">
-                  {showMore
-                    ? descriptionText
-                    : descriptionText.substring(0, 189)}
-                </p>
+          {/* news */}
+          <div className="news__items">
+            <div className="news__item">
+              <div className="news__images">
+                <img src={newsImage} alt="museum" className="news-image" />
               </div>
-              <div className="news__description__time">
-                <p className="news__date">15</p>
-                <p className="news__month">sen</p>
+
+              <div className="news__descriptions">
+                <div className="news__description__info">
+                  <div className="news__description__timestamp">
+                    <h4 className="news-title">Muzey Yangiliklari</h4>
+                    <h4 className="news__location">
+                      <HiLocationMarker className="icon news__location__icon" />
+                      Amaliy san'at muzeyi
+                    </h4>
+                    <h4 className="news-updated-time">
+                      <BsClockFill className="icon " />
+                      day(week): time
+                    </h4>
+                  </div>
+                  <p className="news__description">
+                    {showMore
+                      ? descriptionText
+                      : descriptionText.substring(0, 189)}
+                  </p>
+                </div>
+                <div className="news__description__time">
+                  <p className="news__date">15</p>
+                  <p className="news__month">sen</p>
+                </div>
+              </div>
+            </div>
+            <div className="news__item">
+              <div className="news__images">
+                <img src={newsImage} alt="museum" className="news-image" />
+              </div>
+
+              <div className="news__descriptions">
+                <div className="news__description__info">
+                  <div className="news__description__timestamp">
+                    <h4 className="news-title">Muzey Yangiliklari</h4>
+                    <h4 className="news__location">
+                      <HiLocationMarker className="icon news__location__icon" />
+                      Amaliy san'at muzeyi
+                    </h4>
+                    <h4 className="news-updated-time">
+                      <BsClockFill className="icon " />
+                      day(week): time
+                    </h4>
+                  </div>
+                  <p className="news__description">
+                    {showMore
+                      ? descriptionText
+                      : descriptionText.substring(0, 189)}
+                  </p>
+                </div>
+                <div className="news__description__time">
+                  <p className="news__date">15</p>
+                  <p className="news__month">sen</p>
+                </div>
               </div>
             </div>
           </div>
